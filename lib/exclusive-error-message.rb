d6001267afcb5e8b91c6e4ad3e91a096d24db87d
Yaroslav Konoplov <eahome00@gmail.com>
2018-09-25 16:39:50 +0300

    Add support for non-string error messages like hashes, for example. Bump 1.0.1

--- lib/exclusive-error-message.rb
+++ lib/exclusive-error-message.rb
@@ -4,7 +4,11 @@
 module ExclusiveErrorMessage
   module Extension
     def full_message(attribute, message)
-      message.start_with?('_') ? message[1..-1] : super
+      if message.respond_to?(:start_with?) && message.start_with?('_') # Support non-string error messages like hashes.
+        message[1..-1]
+      else
+        super
+      end
     end
   end
 end
--- lib/exclusive-error-message/version.rb
+++ lib/exclusive-error-message/version.rb
@@ -2,5 +2,5 @@
 # frozen_string_literal: true
 
 module ExclusiveErrorMessage
-  VERSION = '1.0.0'
+  VERSION = '1.0.1'
 end
